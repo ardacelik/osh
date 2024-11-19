b458503672453a70b31965ac81a93bb7cbe03f71
wesleyburnawan <wesley.burnawan@gmail.com>
2024-11-19 02:13:19 -0500

    edit styling

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -47,7 +46,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    <App />
+    <RouterProvider router={router} />    
   </React.StrictMode>
 );
